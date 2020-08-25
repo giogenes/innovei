769c752e47dbb8b27d9d64ff1537667443a32f38
root <root@DESKTOP-492APIP.localdomain>
2020-08-24 21:04:49 -0600

    create basic webserver

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('Hello World!')
+const express = require("express");
+
+const app = express();
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+  res.send(
+    "<h1>Innovei Technologies Web Application API v0.1.0</h1></br><p>Please visit /api/item to get started</p>"
+  );
+});
+
+app.listen(PORT, () => console.log(`backend service started on port ${PORT}`));

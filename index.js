a7f4ba4f3afbae1a984dc2c6282753a7fb728070
Giovanni Leon <giogenes@HAL-9001.localdomain>
2020-08-25 09:22:12 -0600

    add basic manufacturers routing

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require("express");
+const manufacturers = require('./routes/manufacturers.js')
 
 const app = express();
 const PORT = 3000;
+
+app.use(express.json());
+
+app.use('/api/manufacturers', manufacturers);
 
 app.get("/", (req, res) => {
   res.send(
